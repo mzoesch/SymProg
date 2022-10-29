199df7af9f4863346858d3240e76dfb3e40a3aae
mzoesch <magnus.zoeschinger@zoeschinger.de>
2022-10-29 13:35:43 +0200

    bug fixes

--- 20221025/u/oop_1_bank.py
+++ 20221025/u/oop_1_bank.py
@@ -27,68 +27,27 @@ class Account:
     You have to remove the pass statement and then write some
     code for the class. """
 
-    """
-    
-    Class acts as a simple bank account.
-    Money can be deposited and withdrawn.
-    The account holder can be changed.
-
-    """
-
     def __init__(self, account_holder: str, balance: float):
-        """ Constructor of the class. """
+        """
 
-        self.__account_holder: str = account_holder  # private attributes
-        self.__balance: float = balance
-        return
+        Class acts as a simple bank account.\n
+        Money can be deposited and withdrawn.\n
+        The account holder can be changed.
 
-    def deposit(self, amount):
-        """ Deposit money into the account. Negative amounts are not allowed. """
+        """
 
-        if amount <= 0.:
-            print("You cannot deposit negative amounts.")
-            return
-
-        self.__balance += amount
-        return
-
-    def withdraw(self, amount):
-        """ Withdraw money (max 1000€) from the account. Negative amounts are not allowed. """
-
-        if amount <= 0.:
-            print("You cannot withdraw negative amounts.")
-            return
-
-        if amount > 1000.:
-            print("You cannot withdraw more than 1000€ from your account.")
-            return
-
-        if self.__balance - amount < 0.:
-            print("You cannot withdraw more money than you have in your account.")
-            return
-
-        self.__balance -= amount
-        return amount
-
-    def apply_interest(self):
-        """ Apply interest rate of 1.5% to the current balance. """
-
-        self.__balance += self.__balance * 1.015
+        self.__account_holder: str = account_holder  # private attribute
+        self._balance: float = balance
         return
 
     @property
     def account_holder(self):
-        """
-
-        Acts as a getter for __account_holder.
-        A seperate getter method is not needed.
-
-        """
+        """ Acts as a getter for account_holder. A separate getter method is not needed. """
 
         return self.__account_holder
 
     @account_holder.setter
-    def account_holder(self, value):
+    def account_holder(self, value: str):
         """ Setter method for __account_holder. """
 
         self.__account_holder = value
@@ -100,46 +59,91 @@ class Account:
 
     #     return self.__account_holder
 
+    def deposit(self, amount):
+        """ Deposit money into the account. Negative amounts are not allowed. """
+
+        if amount <= 0.:
+            print('You cannot deposit negative amounts.')
+            return
+
+        self._balance += amount
+        return
+
+    def withdraw(self, amount):
+        """ Withdraw money (max 1000€) from the account. Negative amounts are not allowed. """
+
+        # Invalid amounts
+        if amount <= 0:
+            print('You cannot withdraw negative amounts.')
+            return
+        if amount > 1_000:
+            print('You cannot withdraw more than 1,000€ from your account.')
+            return
+
+        # Balance below zero
+        if self._balance <= -1_000:
+            print('You have to mouch debts. You cannot withdraw any money anymore.')
+            return
+        if self._balance - amount < -1_000.:
+            # _balance is always below zero or is zero
+            r, self._balance = 1_000 - abs(self._balance), -1_000
+            print('You can only have a max amount of debts (1,000€) at this bank.')
+            print(f'Withdrawn {r}€ instead.')
+            return r
+
+        self._balance -= amount
+        return amount
+
+    def apply_interest(self, rate: float = 0.015):
+        """ Apply interest rate of 1.5% (default) to current balance. """
+
+        if self._balance <= 0:
+            print('No interest on debts.')
+            return
+        if rate <= 0:
+            print('Interest rate has to be positive.')
+            return
+
+        self._balance += self._balance * rate
+        return self._balance
+
     def __str__(self):
         """ Return a string representation of the account. """
 
-        return f'{self.__account_holder} has {self.__balance} in his account.'
+        return f'{self.account_holder} has {self._balance}€ in their account.'
 
 
 def main():
     """ Main application. """
 
-    account1 = Account("John Smith", 1000.)
-    account2 = Account("Sue Schafer", 2000.)
+    account1 = Account('John Smith', 1_000.)
+    account2 = Account('Sue Schafer', 2_000.)
+    account3 = Account('Max Mustermann', 0.)
 
     print(account1)
-    print(f'{account2}\n')
+    print(account2)
+    print(f'{account3}\n')
 
-    account1.deposit(500.)
-    account2.deposit(-50.)
-
+    account1.withdraw(500.)  # Does not throw
+    account1.withdraw(-50.)  # Negative amount
+    account1.withdraw(2000.)  # To much withdrawn
+    account2.withdraw(1000.)  # Max amount
+    account3.withdraw(500.)  # Negative balance
+    account3.withdraw(700.)  # Max debt
     print(account1)
-    print(f'{account2}\n')
-
-    account1.withdraw(500.)
-    account2.withdraw(-50.)
-    account1.withdraw(2000.)
-    account1.withdraw(500.)
-    account1.withdraw(1000.)
-
-    print(account1)
-    print(f'{account2}\n')
+    print(account2)
+    print(f'{account3}\n')
 
     account1.apply_interest()
     account2.apply_interest()
-
+    account3.apply_interest()  # No interest on debts
     print(account1)
-    print(f'{account2}\n')
+    print(account2)
+    print(f'{account3}\n')
 
     print(f'Account holder is {account1.account_holder}')
     account1.account_holder = "John Doe"
     print(f'Account holder is {account1.account_holder}')
-    print(account1)
 
     return
 
