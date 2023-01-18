2db752c2f3abb632a8fd6b529b9411343e939885
mzoesch <magnus.zoeschinger@zoeschinger.de>
2023-01-18 17:12:19 +0100

    hw10

--- 20230110/hw10_knn/classification.py
+++ 20230110/hw10_knn/classification.py
@@ -1,13 +1,17 @@
 from nltk import FreqDist, word_tokenize
 from collections import defaultdict
-import os, math
+import os
+import math
 from collections import Counter
+
 
 def dot(dict_a, dict_b):
     return sum([dict_a.get(tok) * dict_b.get(tok, 0) for tok in dict_a])
 
+
 def normalized_tokens(text):
     return [token.lower() for token in word_tokenize(text)]
+
 
 class TextDocument:
     def __init__(self, text, id=None, category=None):
@@ -19,11 +23,12 @@ class TextDocument:
     @classmethod
     def from_file(cls, filename, category):
         with open(filename, 'r', encoding="ISO-8859-1") as myfile:
-            text=myfile.read().strip()
+            text = myfile.read().strip()
         return cls(text, filename, category)
 
+
 class DocumentCollection:
-    def __init__(self, term_to_df, term_to_docids, docid_to_doc,doc_to_category):
+    def __init__(self, term_to_df, term_to_docids, docid_to_doc, doc_to_category):
         # string to int
         self.term_to_df = term_to_df
         # string to set of string
@@ -35,7 +40,8 @@ class DocumentCollection:
 
     @classmethod
     def from_dir(cls, dir):
-        files = [(os.path.join(root, name), os.path.relpath(root, dir)) for root, dirs, f in os.walk(dir, topdown=False) for name in f]
+        files = [(os.path.join(root, name), os.path.relpath(root, dir))
+                 for root, dirs, f in os.walk(dir, topdown=False) for name in f]
         docs = [TextDocument.from_file(f, cat) for f, cat in files]
         return cls.from_document_list(docs)
 
@@ -53,10 +59,9 @@ class DocumentCollection:
                 term_to_docids[token].add(doc.id)
         return cls(term_to_df, term_to_docids, docid_to_doc, doc_to_category)
 
-
     def tfidf(self, counts):
         N = len(self.docid_to_doc)
-        return {tok: tf * math.log(N/self.term_to_df[tok]) for tok,tf in counts.items() if tok in self.term_to_df}
+        return {tok: tf * math.log(N/self.term_to_df[tok]) for tok, tf in counts.items() if tok in self.term_to_df}
 
     def cosine_similarity(self, weighted_a, weighted_b):
         dot_ab = dot(weighted_a, weighted_b)
@@ -80,27 +85,88 @@ class KNNClassifier:
                                         for doc in self.doc_collection.docid_to_doc.values()]
 
     def calculate_similarities(self, vec_test_doc, vectors_of_train_docs):
-        #TODO calculate similarities between test and train documents and label them [(similarity, label),...]
-        # have a look at the unittest to see what is the type of vectorsOfTrainDocs
-        pass
+        # TODO calculate similarities between test and train documents and label
+        # TODO them [(similarity, label),...] # -> ok
+        # TODO have a look at the unittest to see what is the type of vectorsOfTrainDocs
+
+        # pass
+
+        similarities = []
+        for vec_train_doc in vectors_of_train_docs:
+            similarities.append(
+                (
+                    self.doc_collection.cosine_similarity(
+                        vec_test_doc, vec_train_doc[1]
+                    ),
+                    vec_train_doc[0].category
+                )
+            )
+
+        return similarities
 
     def order_nearest_to_farthest(self, similarities):
-        #TODO order the labeled points from nearest to farthest
-        pass
+        # TODO order the labeled points from nearest to farthest # -> ok
+
+        # pass
+
+        return sorted(
+            similarities,
+            key=lambda x: x[0],
+            reverse=True
+        )
 
     def labels_k_closest(self, sorted_similarities):
-        #TODO find the labels for the k closest points
-        pass
+        # TODO find the labels for the k closest points # -> ok
 
-    def choose_one(self, labels) :
-        #TODO reduce k until you find a unique winner
-        pass
+        # pass
+
+        return [
+            x[1]
+            for x in sorted_similarities[
+                :self.n_neighbors
+            ]
+        ]
+
+    def choose_one(self, labels):
+        # TODO reduce k until you find a unique winner  # -> ok
+
+        # pass
+
+        return Counter(labels).most_common(1)[0][0]
 
     def classify(self, test_file):
-        #TODO classify test document, have a look at the slides of the course for some help with the order of commands
-        test_doc = TextDocument.from_file(test_file, 'unknowcat')
-        pass
+        # TODO classify test document, have a look at the slides of the course for some help with the order of commands # -> ok
+        # test_doc = TextDocument.from_file(test_file, 'unknowcat')
+
+        # pass
+
+        return self.choose_one(
+            self.labels_k_closest(
+                self.order_nearest_to_farthest(
+                    self.calculate_similarities(
+                        self.doc_collection.tfidf(
+                            TextDocument.from_file(
+                                test_file,
+                                'unknowcat'
+                            ).token_counts
+                        ),
+                        self.vectorsOfDoc_collection
+                    )
+                )
+            )
+        )
 
     def get_accuracy(self, gold, predicted):
-        #TODO calculate accuracy
-        pass
+        # TODO calculate accuracy # -> ok
+
+        # pass
+
+        return sum(
+            [
+                1
+                for x, y in zip(
+                    gold, predicted
+                )
+                if x == y
+            ]
+        ) / len(gold) * 100
